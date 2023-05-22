c39b20b964bbfda35632af950904b04e75cd4000
binhthaitrinh <trinhthaibinh.ecom@gmail.com>
2023-05-21 20:15:00 -0700

    Add RSS feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function get() {
+  return rss({
+    title: 'Astro Learner | Blog',
+    description: 'My journey learning Astro',
+    site: 'https://dazzling-biscochitos-c15edf.netlify.app/',
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
+  });
+}
